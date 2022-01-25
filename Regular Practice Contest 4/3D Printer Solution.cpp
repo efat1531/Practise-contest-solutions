bcf67c1e3a9d46d84fb449802d9df7605454418e
efat1531 <61145086+efat1531@users.noreply.github.com>
2022-01-25 14:21:50 +0600

    Create 3D Printer Solution.cpp

---
+++ Regular Practice Contest 4/3D Printer Solution.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int total_printer = 1, total_days = 0;
+	while (total_printer < n)
+	{
+		total_printer = (total_printer * 2);
+		total_days++;
+	}
+	total_days  = total_days+1;
+	cout << total_days << endl;
+}
