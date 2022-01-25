c8c97af2accebe50a2737da601858f119d68bb2f
efat1531 <61145086+efat1531@users.noreply.github.com>
2022-01-25 14:47:41 +0600

    Add files via upload

---
+++ Regular Practice Contest 4/Adding Reversed Numbers.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int reverse(int n)
+{
+	int sum = 0;
+	while (n > 0)
+	{
+		sum *= 10;
+		sum += (n % 10);
+		n /= 10;
+	}
+	return sum;
+}
+
+int main()
+{
+	int testcase;
+	cin >> testcase;
+	while (testcase--)
+	{
+		int a, b;
+		cin >> a >> b;
+		int sum = reverse(a) + reverse(b);
+		cout << reverse(sum) << endl;
+	}
+}
---
+++ Regular Practice Contest 4/CodeChef-TWOVSTEN.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int testcase;
+	cin >> testcase;
+	while (testcase--)
+	{
+		int n;
+		cin >> n;
+		if (n % 10 == 0)
+			cout << 0 << endl;
+		else if (n % 5 == 0)
+			cout << 1 << endl;
+		else cout << -1 << endl;
+	}
+}
